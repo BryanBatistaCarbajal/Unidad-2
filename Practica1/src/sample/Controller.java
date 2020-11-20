6ba966fa5b6a1b09ec70a3494c92cd38985cbc54
BryanBatistaCarbajal <71846030+BryanBatistaCarbajal@users.noreply.github.com>
2020-11-20 12:42:25 -0700

    Practica1

---
+++ Practica1/src/sample/Controller.java
@@ -0,0 +1,74 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Controller {
+    @FXML TextField txtTexto;
+    @FXML ListView listaEjer;
+    @FXML Label lblResultado;
+    @FXML protected void initialize(){
+        listaEjer.getItems().addAll("Ejercicio 1", "Ejercicio 2");
+    }
+    public void procesar(ActionEvent event){
+        int seleccion = listaEjer.getSelectionModel().getSelectedIndex();
+        String texto = txtTexto.getText();
+        String expresion="";
+        switch (seleccion){
+            case 0:{
+                expresion="[^db]";
+                break;
+            }
+            case 1: {
+                expresion="[count]$";
+                break;
+            }
+            case 2: {
+                expresion="";
+                break;
+            }
+            case 3: {
+                expresion="[a-zA-Z0-9]{12}";
+                break;
+            }
+            case 4: {
+                expresion="^[A-Z][a-bA-Z]$";
+                break;
+            }
+            case 5: {
+                expresion="[0-1]{8}";
+                break;
+            }
+            case 6: {
+                expresion="[A-F0-9]";
+                break;
+            }
+            case 7: {
+                expresion="";
+                break;
+            }
+            case 8: {
+                expresion="^[18CG][0-9]{8}";
+                break;
+            }
+            case 9: {
+                expresion="";
+                break;
+            }
+        }
+        Pattern patron=Pattern.compile(expresion);
+        Matcher matcher=patron.matcher(texto);
+        if (matcher.matches()){
+            lblResultado.setText("SI CUMPLE");
+        }else{
+            lblResultado.setText("NO CUMPLE");
+        }
+    }
+}
